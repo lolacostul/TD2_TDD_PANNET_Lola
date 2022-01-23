4a8ea2eff7bd2e68999990c86fd1cb402a4b609d
lolacostul <lolapnnt@yahoo.com>
2022-01-23 14:35:19 +0100

    Add basic mirror function

--- funcs.py
+++ funcs.py
@@ -1,2 +1,13 @@
 def mirror(word, idx):
-	return -1
+	if isinstance(word, str):
+		if -len(word) <= idx < len(word):
+			first_part = word[0:idx + 1]
+			print(first_part)
+			second_part = first_part[::-1]
+			print(second_part)
+			final_mirrored = first_part + second_part
+			return final_mirrored
+		else:
+			raise IndexError
+	else:
+		raise TypeError
