a55c6284886e1a373fc10462a0c523a8a5619d1a
lolacostul <lolapnnt@yahoo.com>
2022-01-23 14:52:44 +0100

    Test modified with error raising

--- test_funcs.py
+++ test_funcs.py
@@ -21,13 +21,34 @@ class TestFuncs(unittest.TestCase):
 			"HelleH"
 		)
 
+		self.assertEqual(funcs.mirror("bonjour", -1), "rr")
+		self.assertEqual(funcs.mirror("bonjour", -2), "urru")
+		self.assertEqual(funcs.mirror("bonjour", -3), "ourruo")
+		self.assertEqual(funcs.mirror("bonjour", -7), "bonjourruojnob")
+		self.assertEqual(
+			funcs.mirror("Hello World!", -1),
+		 	"!!"
+		 )
+		self.assertEqual(
+			funcs.mirror("Hello World!", -8),
+		 	"o World!!dlroW o"
+		 )
+		self.assertEqual(funcs.mirror(" ", 0), "  ")
+
 		with self.assertRaises(TypeError):
 			funcs.mirror(123456789, 5)
 			funcs.mirror([0,1,2,3,4,5], 4)
 			funcs.mirror(["p","a","o"], 1)
+			funcs.mirror("bonjour", 6, 0)
+			funcs.mirror("Hello World!", 5, "hey")
+			funcs.mirror("Hello World!", 5, [0,1,2])
+			funcs.mirror("Hello World!", [0,1,2])
+
 		with self.assertRaises(IndexError):
 			funcs.mirror("salut", 7)
 			funcs.mirror("", 2)
+			funcs.mirror("", 0)
+			funcs.mirror("bonjour", -8)
 
 if __name__ == '__main__':
 	unittest.main()
