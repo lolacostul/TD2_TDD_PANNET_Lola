5384b30cb9adba1c0e6f8953ab9e1467d91ae915
lolacostul <lolapnnt@yahoo.com>
2022-01-23 14:54:43 +0100

    Mirror function modified to raise all errors and minus index

--- funcs.py
+++ funcs.py
@@ -1,10 +1,11 @@
 def mirror(word, idx):
-	if isinstance(word, str):
+	if isinstance(word, str) and isinstance(idx, int):
 		if -len(word) <= idx < len(word):
-			first_part = word[0:idx + 1]
-			print(first_part)
+			if 0 <= idx < len(word):
+				first_part = word[:idx + 1]
+			elif -len(word) <= idx < 0:
+				first_part = word[idx:]
 			second_part = first_part[::-1]
-			print(second_part)
 			final_mirrored = first_part + second_part
 			return final_mirrored
 		else:
